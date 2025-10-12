c1da2e9c0b09f9a01d231b4d8f7c58feed87c396
amal <amalgiressh007@gmail.com>
2025-10-12 09:48:20 +0530

    class work sql lab

---
+++ work_2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Student WHERE age > 20; 
+SELECT * FROM student WHERE department = 'Computer Science' OR department = 'Physics';
+SELECT * FROM student WHERE grade = 90;
+SELECT * FROM Student WHERE grade BETWEEN 70 AND 90;
