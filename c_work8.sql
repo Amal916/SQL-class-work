3b169c2f5377ef6b791a292269ae38effe547785
Amal916 <amalgireesh007@gmail.com>
2025-10-26 19:51:34 +0530

    classwork day 8

---
+++ c_work8.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE GroceryShop;
+USE GroceryShop;
+CREATE TABLE products ( product_id INT PRIMARY KEY, product_name VARCHAR(100), price DECIMAL(10, 2) );
+ALTER TABLE products ADD COLUMN category VARCHAR(50);
+TRUNCATE TABLE products;
+DROP DATABASE Grocery_Shop;
